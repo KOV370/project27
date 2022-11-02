4edbd753d099206ba454c51b2018b910c07c0f56
KOV370 <kortt370@gmail.com>
2022-11-02 11:46:42 +0200

    2022_11_02 laboratory 06 question about lyamda

---
+++ src/org/laboratory/project27/model/MyInterface.java
@@ -0,0 +1,5 @@
+package org.laboratory.project27.model;
+
+public   interface MyInterface {
+    String  arrayIndex(String line, int array1[], int array2 [],int a);
+}
--- src/org/laboratory/project27/repository/PersonFileRepository.java
+++ src/org/laboratory/project27/repository/PersonFileRepository.java
@@ -1,15 +1,13 @@
 package org.laboratory.project27.repository;
 
 import org.laboratory.project27.concoleUserDialog.ConsoleUserDialog;
+import org.laboratory.project27.model.MyInterface;
 import org.laboratory.project27.model.Person;
 import org.laboratory.project27.model.PersonJob;
-import org.laboratory.project27.runProgram.RunProgram;
 
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 
-public class PersonFileRepository {
+public class PersonFileRepository implements MyInterface {
     private static final String FILE = "C:\\JetBrains Projects\\Project27_laboratory.txt";
 
 
@@ -60,22 +58,37 @@ public class PersonFileRepository {
         int firstIndex = 0;
         int secondIndex = 0;
         for (int i = 0; i < 5; i++) {
-            firstIndex =  line.indexOf("=", firstIndex+1);
+            firstIndex = line.indexOf("=", firstIndex + 1);
             arraysFirst[i] = firstIndex;
         }
-        {for (int j = 0; j < 4; j++) {
-            secondIndex = line.indexOf(",", secondIndex+1);
-            arraysEnd[j] = secondIndex;
+        {
+            for (int j = 0; j < 4; j++) {
+                secondIndex = line.indexOf(",", secondIndex + 1);
+                arraysEnd[j] = secondIndex;
+            }
+            arraysEnd[4] = line.length();
         }
-        arraysEnd[4] = line.length();}
         String firstName = line.substring(arraysFirst[0] + 1, arraysEnd[0]);
         String lastName = line.substring(arraysFirst[1] + 1, arraysEnd[1]);
         int birthYear = Integer.parseInt(line.substring(arraysFirst[2] + 1, arraysEnd[2]));
         PersonJob job = PersonJob.valueOf(line.substring(arraysFirst[3] + 1, arraysEnd[3]));
-        double salary =Double.parseDouble(line.substring(arraysFirst[4] + 1, arraysEnd[4]));
+        double salary = Double.parseDouble(line.substring(arraysFirst[4] + 1, arraysEnd[4]));
         Person person = new Person(firstName, lastName, birthYear, job, salary);
         System.out.println(person);
         return person;
+    }
+
+
+//    @Override
+//    public String arrayIndex(String line, String[] array1, String[] array2, int a, int b) {
+//        String subLine = ( ) -> line.substring(array1[a] + 1, array2[b])
+//        return subLine;
+//    }
+
+    @Override  //todo хочу сделать типовой метод для нахождения параметров  в строке - не пойму что ему не нравится
+    public String arrayIndex(String line, int[] array1, int[] array2, int a) {
+        String subLine = ( ) -> line.substring(array1[a] + 1, array2[a]);
+        return subLine;
     }
 }
 
