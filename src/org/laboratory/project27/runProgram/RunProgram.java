2c6084b1ab7cbb420d7112ba8a079e9b6d6832c5
Serhii Mykhliuk <serhii.mykhliuk@edcast.com>
2022-11-08 18:39:19 +0200

    review 22_11_08

--- src/org/laboratory/project27/runProgram/RunProgram.java
+++ src/org/laboratory/project27/runProgram/RunProgram.java
@@ -5,8 +5,6 @@ import org.laboratory.project27.repository.PersonFileRepository;
 import org.laboratory.project27.model.Person;
 import org.laboratory.project27.service.PersonService;
 
-import java.util.InputMismatchException;
-
 public class RunProgram {
     private static final String MENU = """
             Menu
@@ -15,59 +13,73 @@ public class RunProgram {
             3-find record from the file
             9-exit     
             """;
-    public static int numberMenu;
-    static Person correctPerson = new Person();//чтобы новая запись сохрянялась после создания и ожидала либо записи
-    //в файл либо продолжения
-    static Person person = null;
-    private final ConsoleUserDialog ui = new ConsoleUserDialog();
-    private final PersonFileRepository personFileRepository = new PersonFileRepository();
-    private final PersonService personService = new PersonService(ui,personFileRepository);
+    public static final boolean PROGRAM_EXIT = false;
+    public static final boolean CONTINUE_EXECUTION = true;
+
+    private Person currentPerson;
+
+    private final ConsoleUserDialog ui;
+    private final PersonFileRepository personFileRepository;
+    private final PersonService personService;
+
+    public RunProgram() {
+        ui = new ConsoleUserDialog();
+        personFileRepository = new PersonFileRepository();
+        personService = new PersonService(ui, personFileRepository);
+    }
 
     public static void main(String[] args) {
         new RunProgram().runMenu();
     }
 
     private void runMenu() {
+        boolean continueExecution;
         ui.printMessage(MENU);
-        while (true) {
-            try {
-                numberMenu = ui.readInt("Make your choice.");
-                choiceMenu(numberMenu);
-            } catch (InputMismatchException r) {
-                System.out.println("Enter right number");//todo как вернуться в метод при неправильном формате?
-                return;
-            }
-        }
+        do {
+            continueExecution = choiceMenu();
+        } while (continueExecution);
     }
+    private boolean choiceMenu() {
+        ui.printMessage("==============================");
+        ui.printMessage("Current person is: " + currentPerson);
+        int numberMenu = ui.readInt("Make your choice");
 
-    private void choiceMenu(int number) {
-        switch (number) {
+        switch (numberMenu) {
             case 1:
-                person = personService.createNewPerson();
-                if (person == null) {
-                    return;
-                } else {
-                    correctPerson = person;
-                    break;
-                }
+                createPerson();
+                break;
             case 2:
-                PersonFileRepository.unloadToFife(String.valueOf(correctPerson), true);
-                //todo убрать ссылку на PersonFileRepository заменить на метод personService.create()
+                savePerson();
                 break;
             case 3:
-                personService.getPersonByName(ui.enterString("Enter the name for downloading."));
+                findPersonByName();
                 break;
             case 9:
-                System.exit(0);
+                return PROGRAM_EXIT;
             default:
-                System.out.println("Mistake with entering.");
+                System.out.println("No menu item found"); //todo use ui.printMessage()
                 break;
+        }
+        return CONTINUE_EXECUTION;
+    }
+
+    private void createPerson() {
+        currentPerson = personService.createNewPerson();
+    }
+
+    private void savePerson() {
+        PersonFileRepository.unloadToFife(String.valueOf(currentPerson), true);
+        //todo убрать ссылку на PersonFileRepository заменить на метод personService.create()
+    }
+
+    private void findPersonByName() {
+        Person person = personService.getPersonByName(ui.enterString("Enter the name for downloading."));
+
+        if (person == null) {
+            ui.printMessage("Person not found");
+        } else {
+            ui.printMessage(person.toString());
         }
     }
 
-
 }
-
-
-
-
