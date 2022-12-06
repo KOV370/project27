7034e2b060980d06a89a0ef1cd0416f05ed0894e
KOV370 <kortt370@gmail.com>
2022-12-06 17:50:51 +0200

    2022_12_06 test changed test 3

--- src/main/java/org/laboratory/project27/example/RunExample.java
+++ src/main/java/org/laboratory/project27/example/RunExample.java
@@ -8,10 +8,10 @@ import java.util.Scanner;
 public class RunExample {
 
     public static void main(String[] args) {
-        System.out.println("Test 1");
 
         Scanner scanner = new Scanner(System.in);
-        //            double a,b,c = 0;
+//        System.out.println("Test 1");
+//                   double a,b,c = 0;
 //        if (scanner.hasNextDouble()) {
 //            a = scanner.nextDouble();
 //            b = scanner.nextDouble();
@@ -40,93 +40,77 @@ public class RunExample {
 //        }
 
 
-//        System.out.println("Test 3");
-//        int a, b, c, d;
-//        int min = 0, max = 0, middle = 0;
-//        if (scanner.hasNextInt()) {
-//            {
-//                a = scanner.nextInt();
-//                b = scanner.nextInt();
-//                c = scanner.nextInt();
-//                scanner.close();
-//            }
-//            System.out.println(a + " " + b + " " + c);
-//            if (a < b) {
-//                if (a < c) {
-//                    min = a;
-//                    max = Math.max(b, c);
-//                    middle = Math.min(b, c);
-//                } else {
-//                    min = c;
-//                    max = Math.max(a, b);
-//                    middle = Math.min(a, b);
-//                }
-//            } else {
-//                min = Math.min(b, c);
-//                max = Math.max(a, c);
-//                if (max == c) {
-//                    middle = a;
-//                } else {
-//                    middle = Math.max(b, c);
-//                }
-//            }
-//        }
-//        a = min;
-//        b = middle;
-//        c = max;
-//        System.out.println(a + " " + b + " " + c);
-
-        System.out.println("Test 4");
-        boolean a = false;
-        boolean b = false;
-        boolean c = false;
-        int countTrue = 0;
-        System.out.print("Enter three boolean " +
-                "values (true or false) ");
-        if (scanner.hasNextBoolean()) {
-            a = scanner.nextBoolean();
-            if (a) {
-                countTrue += 1;
-            }
-            b = scanner.nextBoolean();
-            if (b) {
-                countTrue += 1;
-            }
-            c = scanner.nextBoolean();
-            if (c) {
-                countTrue += 1;
-            }
+        System.out.println("Test 3");
+        int a = 0, b = 0, c = 0, average;
+        int min, max, middle;
+        if (scanner.hasNextInt()) {
+            a = scanner.nextInt();
+            b = scanner.nextInt();
+            c = scanner.nextInt();
             scanner.close();
         }
-        System.out.println(countTrue);
-        boolean allThree = false;
-        boolean exactlyOne = false;
-        boolean exactlyTwo = false;
-        boolean atLeastOne = false;
-        boolean atLeastTwo = false;
-        if (countTrue == 3) {
-            allThree = true;
-        }
-        if (countTrue == 1) {
-            exactlyOne = true;
-        }
-        if (countTrue == 2) {
-            exactlyTwo = true;
-        }
-        if (countTrue >= 1) {
-            atLeastOne = true;
-        }
-        if (countTrue >= 2) {
-            atLeastTwo = true;
-        }
-        System.out.println("a, b, c = " + a + ", " + b +
-                ", " + c + "\nallThree: " + allThree +
-                "\nexactlyOne: " + exactlyOne +
-                "\nexactlyTwo: " + exactlyTwo +
-                "\natLeastOne: " + atLeastOne +
-                "\natLeastTwo: " + atLeastTwo);
+        System.out.println(a + " " + b + " " + c);
+        min = Math.min(Math.min(a, b), c);
+        max = Math.max(Math.max(a, b), c);
+        middle = a+b+c-min-max;
+        a = min;
+        b = middle;
+        c = max;
+        System.out.println(a + " " + b + " " + c);
+
+//        System.out.println("Test 4");
+//        boolean a = false;
+//        boolean b = false;
+//        boolean c = false;
+//        int countTrue = 0;
+//        System.out.print("Enter three boolean " +
+//                "values (true or false) ");
+//        if (scanner.hasNextBoolean()) {
+//            a = scanner.nextBoolean();
+//            if (a) {
+//                countTrue += 1;
+//            }
+//            b = scanner.nextBoolean();
+//            if (b) {
+//                countTrue += 1;
+//            }
+//            c = scanner.nextBoolean();
+//            if (c) {
+//                countTrue += 1;
+//            }
+//            scanner.close();
+//        }
+//        System.out.println(countTrue);
+//        boolean allThree = false;
+//        boolean exactlyOne = false;
+//        boolean exactlyTwo = false;
+//        boolean atLeastOne = false;
+//        boolean atLeastTwo = false;
+//        if (countTrue == 3) {
+//            allThree = true;
+//        }
+//        if (countTrue == 1) {
+//            exactlyOne = true;
+//        }
+//        if (countTrue == 2) {
+//            exactlyTwo = true;
+//        }
+//        if (countTrue >= 1) {
+//            atLeastOne = true;
+//        }
+//        if (countTrue >= 2) {
+//            atLeastTwo = true;
+//        }
+//        System.out.println("a, b, c = " + a + ", " + b +
+//                ", " + c + "\nallThree: " + allThree +
+//                "\nexactlyOne: " + exactlyOne +
+//                "\nexactlyTwo: " + exactlyTwo +
+//                "\natLeastOne: " + atLeastOne +
+//                "\natLeastTwo: " + atLeastTwo);
+//    }
     }
 }
+
 
 
 
